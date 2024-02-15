b23d6d99d4e6683720af1922a9ce841e2fe0332f
Enikish <www.1374915762@foxmail.com>
2024-02-15 11:22:40 +0800

    URLconf

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,11 +7,18 @@ from . import views
 # route不会匹配get和post参数或者域名。例如在请求127.0.0.1:8000/polls/index和127.0.0.1:8000/polls/index?page=3时，都只会匹配~/polls/index
 # view: 当django找到匹配的url后，就会调用view这个特定的视图函数，并传入一个HttpRequest对象作为第一个参数，被捕获的参数以关键字参数的形式传入
 app_name = "polls"  # 添加命名空间
+
+# urlpatterns = [
+#     # path("index", views.index),
+#     path("recall", views.recall, name="recall"),
+#     path("", views.index, name="index"),
+#     path("<int:question_id>/results/", views.results, name="results"),
+#     path("<int:question_id>/vote/", views.vote, name="vote"),
+#     path("detail/<int:question_id>/", views.detail, name="detail"),
+# ]
 urlpatterns = [
-    # path("index", views.index),
-    path("recall", views.recall, name="recall"),
-    path("", views.index, name="index"),
-    path("<int:question_id>/results/", views.results, name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
-    path("detail/<int:question_id>/", views.detail, name="detail"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Question
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 # Create your views here.
@@ -28,12 +49,35 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the result of question %s."
-    return HttpResponse(response % question_id)
+    # response = "You're looking at the result of question %s."
+    # return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
-def vote(request, question_id: int):
-    return HttpResponse("You're voting on question %s." % question_id)
+def vote(request, question_id):
+    # return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice."
+            }
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        template = loader.get_template("polls/results.html")
+        context = {
+            "question.id": question.id,
+        }
+        # 为防止用户点击回退按钮的情况下数据被post两次，所以每次处理完成POST数据后，都需要返回一个HttpResponseRedirect
+        return HttpResponse(reverse("polls:results", args=(question.id,)))
 
 
 def index(request):
