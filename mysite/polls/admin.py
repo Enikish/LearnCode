0c8adafa702dbb6c6015b9f298a1bd10d9c34105
Enikish <www.1374915762@foxmail.com>
2024-02-16 23:12:29 +0800

    URLconf

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,11 +5,17 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Question name", {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
